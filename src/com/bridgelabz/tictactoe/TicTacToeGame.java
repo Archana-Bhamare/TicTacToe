11035fb59025f65b7bcec8ce48bac251b9c19890
Archana-Bhamare <archusonar2@gmail.com>
2020-11-03 16:26:56 +0530

    Added : Choosing Letter from User

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	public static Scanner sc = new Scanner(System.in);
+	public static char player, computer;
+
 	private void createBoard() {
 		char[] board = new char[10];
 		for (int i = 1; i < 10; i++) {
@@ -8,9 +13,20 @@ public class TicTacToeGame {
 		}
 	}
 
+	private char chooseLetter() {
+		System.out.println("Enter the Letter");
+		return sc.next().charAt(0);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe program");
 		TicTacToeGame ticTacToe = new TicTacToeGame();
 		ticTacToe.createBoard();
+		ticTacToe.chooseLetter();
+		if (player == 'X') {
+			computer = 'O';
+		} else {
+			computer = 'X';
+		}
 	}
 }
